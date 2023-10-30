af7c47423386f2dc6bd7d4f2e1686787c7598642
SYNTAX <kavinduchamath02@gmail.com>
2023-10-30 09:25:32 +0530

    New Files

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeview() {
+
+    var signin = document.getElementById("signinbox");
+    var signup = document.getElementById("signupbox");
+
+    signin.classList.toggle("d-none");
+    signup.classList.toggle("d-none");
+
+}
